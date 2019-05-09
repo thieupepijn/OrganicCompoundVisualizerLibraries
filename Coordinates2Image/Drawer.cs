37148e30ada7a02382a0aca866d3df46a2557b73
thieupepijn <thieupepijn@hotmail.com>
2019-05-09 21:58:16 +0200

    ninth commit

--- Coordinates2Image/Drawer.cs
+++ Coordinates2Image/Drawer.cs
@@ -23,17 +23,45 @@ namespace Coordinates2Image
 		{
 			Bitmap bitmap = new Bitmap(imageWidth, imageHeight);
 			
-			using (Graphics grafix = Graphics.FromImage(bitmap))
+			using (Graphics graafix = Graphics.FromImage(bitmap))
 			{
-				foreach(Node node in nodes)
-				{
-					grafix.DrawEllipse(new Pen(Color.Red, 5), node.Location.X, node.Location.Y, 50, 50);
-					
-					bitmap.Save(imageFilePath, ImageFormat.Bmp);
-				}
+				DrawNodes(nodes, graafix);
+				DrawVertices(vertices, graafix);
+				bitmap.Save(imageFilePath, ImageFormat.Bmp);
 			}
 			
 		}
+		
+		
+		private void DrawNodes(List<Node> nodes, Graphics graafix)
+		{
+			foreach(Node node in nodes)
+			{
+				graafix.FillEllipse(Brushes.Red, node.Location.X, node.Location.Y, 25, 25);
+			}
+		}
+		
+		
+		private void DrawVertices(List<Vertice> vertices, Graphics graafix)
+		{
+			foreach(Vertice vertice in vertices)
+			{
+				int x1 = Average(vertice.Node1.Location.X, vertice.Node1.Location.X + 25);
+				int y1 = Average(vertice.Node1.Location.Y, vertice.Node1.Location.Y + 25);
+				
+				int x2 = Average(vertice.Node2.Location.X, vertice.Node2.Location.X + 25);
+				int y2 = Average(vertice.Node2.Location.Y, vertice.Node2.Location.Y + 25);
+						
+				graafix.DrawLine(new Pen(Color.Red, 2), x1, y1, x2, y2);
+			}
+		}
+		
+		private int Average(int start, int end)
+		{
+			return start +  (Math.Abs(start - end) / 2);
+		}
+		
+	
 		
 		
 		
