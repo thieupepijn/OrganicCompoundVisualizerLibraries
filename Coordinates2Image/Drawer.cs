1c1331873198fc8381732e3740f940868d84c17e
thieupepijn <thieupepijn@hotmail.com>
2019-05-15 23:26:49 +0200

    twelfth commit

--- Coordinates2Image/Drawer.cs
+++ Coordinates2Image/Drawer.cs
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Windows.Media.Imaging;
+using System.IO;
 using Graph2Coordinates;
 
 
@@ -19,17 +21,53 @@ namespace Coordinates2Image
 	public class Drawer
 	{
 		
-		public Drawer(List<Node> nodes, List<Vertice> vertices, int imageWidth, int imageHeight, string imageFilePath)
+		private List<Node> _nodes;
+		private List<Vertice> _vertices;
+		private int _imageWidth;
+		private int _imageHeight;
+		
+		public Drawer(List<Node> nodes, List<Vertice> vertices, int imageWidth, int imageHeight)
 		{
-			Bitmap bitmap = new Bitmap(imageWidth, imageHeight);
-			
+			_nodes = nodes;
+			_vertices = vertices;
+			_imageWidth = imageWidth;
+			_imageHeight = imageHeight;
+		}
+		
+		
+		public Bitmap Draw2Bitmap()
+		{
+			Bitmap bitmap = new Bitmap(_imageWidth, _imageHeight);
 			using (Graphics graafix = Graphics.FromImage(bitmap))
 			{
-				DrawNodes(nodes, graafix);
-				DrawVertices(vertices, graafix);
-				bitmap.Save(imageFilePath, ImageFormat.Bmp);
+				DrawNodes(_nodes, graafix);
+				DrawVertices(_vertices, graafix);
+				return bitmap;
 			}
+		}
+		
+		public BitmapImage Draw2BitmapImage()
+		{
+			Bitmap bitmap = Draw2Bitmap();
 			
+			using(MemoryStream memory = new MemoryStream())
+			{
+				bitmap.Save(memory, ImageFormat.Png);
+				memory.Position = 0;
+				BitmapImage bitmapImage = new BitmapImage();
+				bitmapImage.BeginInit();
+				bitmapImage.StreamSource = memory;
+				bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+				bitmapImage.EndInit();
+				return bitmapImage;
+			}		
+		}
+		
+		
+		public void Draw2File(string imageFilePath)
+		{
+			Bitmap bitmap = Draw2Bitmap();
+			bitmap.Save(imageFilePath, ImageFormat.Bmp);
 		}
 		
 		
@@ -51,7 +89,7 @@ namespace Coordinates2Image
 				
 				int x2 = Average(vertice.Node2.Location.X, vertice.Node2.Location.X + 25);
 				int y2 = Average(vertice.Node2.Location.Y, vertice.Node2.Location.Y + 25);
-						
+				
 				graafix.DrawLine(new Pen(Color.Red, 2), x1, y1, x2, y2);
 			}
 		}
@@ -61,7 +99,7 @@ namespace Coordinates2Image
 			return start +  (Math.Abs(start - end) / 2);
 		}
 		
-	
+		
 		
 		
 		
