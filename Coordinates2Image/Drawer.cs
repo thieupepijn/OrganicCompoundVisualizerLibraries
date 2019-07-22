e1542146e9e1c12e073ef6ff9de53371b1ba9a71
thieupepijn <thieupepijn@hotmail.com>
2019-07-22 23:29:03 +0200

    thirtieth commit

--- Coordinates2Image/Drawer.cs
+++ Coordinates2Image/Drawer.cs
@@ -35,7 +35,7 @@ namespace Coordinates2Image
 			_imageWidth = imageWidth;
 			_imageHeight = imageHeight;
 			_drawingColor = drawingColor;
-			_backgroundColor = backGroundColor;					
+			_backgroundColor = backGroundColor;
 		}
 		
 		
@@ -49,23 +49,25 @@ namespace Coordinates2Image
 			_backgroundColor = Color.White;
 		}
 		
-				
+		
 		public Bitmap Draw2Bitmap()
 		{
 			Bitmap bitmap = new Bitmap(_imageWidth, _imageHeight);
-		
+			
 			using (Graphics graafix = Graphics.FromImage(bitmap))
 			{
 				graafix.Clear(_backgroundColor);
-				DrawNodes(_nodes, graafix);
-				DrawVertices(_vertices, graafix);
+				FontFamily fontFamily = new FontFamily("Arial");
+				Font font = new Font(fontFamily, 25, FontStyle.Regular, GraphicsUnit.Pixel);
+				DrawVertices(_vertices, graafix, font);
+				DrawNodes(_nodes, graafix, font);
 				return bitmap;
 			}
 		}
 		
 		public BitmapImage Draw2BitmapImage()
 		{
-			Bitmap bitmap = Draw2Bitmap();			
+			Bitmap bitmap = Draw2Bitmap();
 			using(MemoryStream memory = new MemoryStream())
 			{
 				bitmap.Save(memory, ImageFormat.Png);
@@ -76,7 +78,7 @@ namespace Coordinates2Image
 				bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
 				bitmapImage.EndInit();
 				return bitmapImage;
-			}		
+			}
 		}
 		
 		
@@ -87,24 +89,40 @@ namespace Coordinates2Image
 		}
 		
 		
-		private void DrawNodes(List<Node> nodes, Graphics graafix)
+		private void DrawNodes(List<Node> nodes, Graphics graafix, Font font)
 		{
 			foreach(Node node in nodes)
 			{
-				graafix.FillEllipse(_drawingColor, node.Location.X, node.Location.Y, 25, 25);
+				int x, y, width, height;
+				GetDrawingLocationAndSize(node, graafix, font, out x, out y, out width, out height);
+				graafix.FillEllipse(Brushes.Pink, x, y, width, height);
+				graafix.DrawString("C", font, _drawingColor, x, y);
 			}
 		}
 		
+		private void GetDrawingLocationAndSize(Node node, Graphics graafix, Font font, out int x, out int y, out int width, out int height)
+		{
+			SizeF size = graafix.MeasureString("C", font);
+			x = (int)(node.Location.X - (0.5 * size.Width));
+			y = (int)(node.Location.Y - (0.5 * size.Height));
+			width = (int)size.Width;
+			height = (int)size.Height;
+		}
 		
-		private void DrawVertices(List<Vertice> vertices, Graphics graafix)
+		
+		private void DrawVertices(List<Vertice> vertices, Graphics graafix, Font font)
 		{
 			foreach(Vertice vertice in vertices)
 			{
-				int x1 = Average(vertice.Node1.Location.X, vertice.Node1.Location.X + 25);
-				int y1 = Average(vertice.Node1.Location.Y, vertice.Node1.Location.Y + 25);
+				int x1, y1, width1, height1;
+				GetDrawingLocationAndSize(vertice.Node1, graafix, font, out x1, out y1, out width1, out height1);
+				x1 += (int)(width1 / 2);
+				y1 += (int)(height1 / 2);
 				
-				int x2 = Average(vertice.Node2.Location.X, vertice.Node2.Location.X + 25);
-				int y2 = Average(vertice.Node2.Location.Y, vertice.Node2.Location.Y + 25);
+				int x2, y2, width2, height2;
+				GetDrawingLocationAndSize(vertice.Node2, graafix, font, out x2, out y2, out width2, out height2);
+				x2 += (int)(width2 / 2);
+				y2 += (int)(height2 / 2);
 				
 				graafix.DrawLine(new Pen(_drawingColor, 2), x1, y1, x2, y2);
 			}
